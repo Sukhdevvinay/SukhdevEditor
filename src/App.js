4316157dcb96344c242e901998e58241c415d90c
Sukhdevvinay <129602601+Sukhdevvinay@users.noreply.github.com>
2026-03-01 00:31:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import Editor from "./Component/Editor"
 import Draw from "./Component/Draw"
 import Login from "./Component/Login"
 import Signup from "./Component/Signup"
-import { Routes,Route } from "react-router";
+import { Routes, Route } from "react-router";
 function App() {
   return (
     <>
       <Routes>
-          <Route path = "/" element = {<Login/>} />;
-          <Route path = "/editor" element = {<Editor/>}/> 
-          <Route path = "/Draw" element = {<Draw/>}/> 
-          <Route path = "/Signup" element = {<Signup/>}/>
-          <Route path = "/logout" element = {<Login/>}/>
+        <Route path="/" element={<Login />} />;
+        <Route path="/editor" element={<Editor />} />
+        <Route path="/Draw" element={<Draw />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/logout" element={<Login />} />
       </Routes>
     </>
   )
