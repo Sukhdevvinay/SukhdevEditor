6c05a743a7e2a30ee16035c04b289f55b0697c04
Sukhdevvinay <129602601+Sukhdevvinay@users.noreply.github.com>
2026-03-01 00:34:57 +0530

    Update save_text.js

--- Backend/Routes/save_text.js
+++ Backend/Routes/save_text.js
@@ -8,24 +8,24 @@ const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
 
-router.post('/save_text',isLoggedIn,async (req, res) => {
- let user_email = req.User_data.email; // Cookie se Aye email 
- const text_data = req.body.Text_data; // Frontend se Aye Text_data
-//  console.log("Actuals rec body : ",req.body);
-//  console.log("Recieved data : ",text_data);
+router.post('/save_text', isLoggedIn, async (req, res) => {
+  let user_email = req.User_data.email; // Cookie se Aye email 
+  const text_data = req.body.Text_data; // Frontend se Aye Text_data
+  //  console.log("Actuals rec body : ",req.body);
+  //  console.log("Recieved data : ",text_data);
   // try {
-    const user = await User.findOne({email:user_email});
-    if (!user) { // User is Not exist 
-      return res.status(401).json({ message: 'Create Your Account' });
-    } else { // User Exits
-      // console.log("Recieved data Else [Part]: ",text_data)
-      user.Text_data = text_data;
-      // console.log("Recived Data in Backedn : ",text_data);
-      await user.save();
-      // console.log(user);               
-      res.status(200).json({ message: "Updated"});
-      // return res.status(200).json({ message: "Text data saved successfully"});
-    }
+  const user = await User.findOne({ email: user_email });
+  if (!user) { // User is Not exist 
+    return res.status(401).json({ message: 'Create Your Account' });
+  } else { // User Exits
+    // console.log("Recieved data Else [Part]: ",text_data)
+    user.Text_data = text_data;
+    // console.log("Recived Data in Backedn : ",text_data);
+    await user.save();
+    // console.log(user);               
+    res.status(200).json({ message: "Updated" });
+    // return res.status(200).json({ message: "Text data saved successfully"});
+  }
   // } catch (err) {
   //   console.log("Server Error");
   //   return res.status(500).json({ message: 'Server error', error: err });
@@ -33,16 +33,16 @@ router.post('/save_text',isLoggedIn,async (req, res) => {
 });
 
 function isLoggedIn(req, res, next) {
-    const token = req.cookies.token;
-    if (!token || token === "") {
-      return res.status(401).json({ error: "Unauthorized. Please log in." });
-    }
-    try {
-      const data = jwt.verify(token, "privatestring");
-      req.User_data = data;
-      next(); 
-    } catch(err) {
-       return res.status(403).json({ error: "Invalid or expired token" });
-    }
+  const token = req.cookies.token;
+  if (!token || token === "") {
+    return res.status(401).json({ error: "Unauthorized. Please log in." });
+  }
+  try {
+    const data = jwt.verify(token, "privatestring");
+    req.User_data = data;
+    next();
+  } catch (err) {
+    return res.status(403).json({ error: "Invalid or expired token" });
+  }
 }
 module.exports = router;
